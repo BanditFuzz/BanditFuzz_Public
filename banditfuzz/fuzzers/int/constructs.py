1c7fd99b67b72fc03e656150adb8e46091bfa7d6
Xiaoyang (Alex) Hong <41179058+obvious666@users.noreply.github.com>
2020-07-25 22:37:34 -0400

    Add files via upload

---
+++ banditfuzz/fuzzers/int/constructs.py
@@ -0,0 +1,169 @@
+class NEG:
+    def __init__(self):
+        self.arity = 1
+        self.sort = 'int'
+        self.sig = [
+            'int',
+            'int'
+        ]
+        self.chainable = False
+
+    def __str__(self):
+        return "-"
+
+    __repr__ = __str__
+
+
+
+class ADD:
+    def __init__(self):
+        self.arity = 2
+        self.sort = 'int'
+        self.sig = [
+            'int', 'int',
+            'int'
+        ]
+        self.chainable = False
+
+    def __str__(self):
+        return "+"
+
+    __repr__ = __str__
+
+
+class SUB:
+    def __init__(self):
+        self.arity = 2
+        self.sort = 'int'
+        self.sig = [
+            'int',
+            'int',
+            'int'
+        ]
+        self.chainable = False
+
+    def __str__(self):
+        return "-"
+
+    __repr__ = __str__
+
+
+class MUL:
+    def __init__(self):
+        self.arity = 2
+        self.sort = 'int'
+        self.sig = [
+            'int', 'int', 'int'
+        ]
+        self.chainable = False
+
+    def __str__(self):
+        return "*"
+
+    __repr__ = __str__
+
+
+class DIV:
+    def __init__(self):
+        self.arity = 2
+        self.sort = 'int'
+        self.sig = [
+            'int', 'int', 'int'
+        ]
+        self.chainable = False
+
+    def __str__(self):
+        return "/"
+
+    __repr__ = __str__
+
+class MOD:
+    def __init__(self):
+        self.arity = 2
+        self.sort = 'int'
+        self.sig = [
+            'int', 'int', 'int'
+        ]
+        self.chainable = False
+
+    def __str__(self):
+        return "mod"
+
+    __repr__ = __str__
+
+class REM:
+    def __init__(self):
+        self.arity = 2
+        self.sort = 'int'
+        self.sig = [
+            'int', 'int', 'int'
+        ]
+        self.chainable = False
+
+    def __str__(self):
+        return "rem"
+
+    __repr__ = __str__
+
+class LEQ:
+    def __init__(self):
+        self.arity = 2
+        self.sort = 'bool'
+        self.sig = [
+            'int', 'int',
+            'bool'
+        ]
+        self.chainable = False
+
+    def __str__(self):
+        return "<="
+
+    __repr__ = __str__
+
+
+class LT:
+    def __init__(self):
+        self.arity = 2
+        self.sort = 'bool'
+        self.sig = [
+            'int', 'int',
+            'bool'
+        ]
+        self.chainable = False
+
+    def __str__(self):
+        return "<"
+
+    __repr__ = __str__
+
+
+class GEQ:
+    def __init__(self):
+        self.arity = 2
+        self.sort = 'bool'
+        self.sig = [
+            'int', 'int',
+            'bool'
+        ]
+        self.chainable = False
+
+    def __str__(self):
+        return ">="
+
+    __repr__ = __str__
+
+
+class GT:
+    def __init__(self):
+        self.arity = 2
+        self.sort = 'bool'
+        self.sig = [
+            'int', 'int',
+            'bool'
+        ]
+        self.chainable = False
+
+    def __str__(self):
+        return ">"
+
+    __repr__ = __str__
